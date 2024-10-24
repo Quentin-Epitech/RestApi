d3b399c833b28a8a5644dff290af4b0d657de0c0
Quentin-Epitech <quentin.bonnet@epitech.digital>
2024-10-24 10:10:48 +0200

    restoapi

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# RestApi
+# Pour ce projet RestApi,nous avons plusieurs requêtes dans notre index.js dans le but d'envoyer differentes requête avec Postman
+# sur notre DB,pour notre LocalHost du serveur,nous allons utiliser MAMP.
+
+=> Dans notre index.js,les ligne 1 à 20 permettent de configurer notre projet.
+    Soit importer le module express,le module MYSQL et de crée l'instance app pour definir nos routes de nos API.
+
+    On va aussi définir un port où Express va fonctionner avec "const expressPort = 3000"
+
+    Ensuite,const dataBase = mysql.createConnection va crée  la connexion avec la BD MYSQL,et database.connect va envoyer la requête 
+    de connexion.
+
+=> App.listen nous permet ensuite de nous confirmer que le serveur tourne bien sur le bon port.
+
+# Ensuite,nous avons toutes nos routes que nous allons utiliser avec Postman.
+
+=>App.get/items va recuperer toute les lignes de la colonne items avec sa requête SQL via la route http://localhost:3000/items 
+  en passant par Postman en créeant notre route avec ce lien,et va nous renvoyer la reponse dans le body .
+  elle va aussi gerer les erreurs en renvoyant un message si il y a un erreur,et aussi si tout est bon.
+
+=>app.post/caca va être une route pour crée une nouvelle colonne dans la table items grâce à la requête SQL.
+Dans postman,nous devons configurer comme avant cette route,et dans le body choisir raw et donner nos infos que nous voulons
+mettre dans cette colonne.Par exemple : {"name": "Mousse au chocolat","prix" : 600 ,"id_category": 2,"description":"mousse"},
+Puis,la requête va inserer dans la table items cette colonne.Avec un traitement du resultat dans la console.
+
+=>app.put/update va être du même principe,mais elle va elle modifié les données d'une colonne déjà existante.
+
+=>app.delete/delete est aussi du même principe,mais elle va supprimer une colonne demandé en donnant son id.
+
+=>app.post/item-category va elle gerer l'ajout des id de Items et de category dans la table item_category.
+Elle va chercher l'id de du nom donné dans postman,puis va extraire le premier resultat donné par la requête.
+elle va ensuite inserer ces données dans item_category .
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,99 @@
+const express = require('express')
+const mysql = require('mysql')
+const app = express()
+const expressPort = 3000
+
+
+app.use(express.json())
+const dataBase = mysql.createConnection(
+    {
+        host:'localhost',
+        user: 'root',
+        port: '3306',
+        password: 'root',
+        database:'restapi'
+    })
+
+dataBase.connect((err) => {
+    if (err) {console.log("Erreur de connexion")}
+       {console.log("bravo,vous êtes connectés")}
+})
+
+
+
+app.listen(expressPort,() => {
+    console.log('serveur tourne sur le port:' , expressPort)})
+
+app.get("/ping",(req,res)=>{
+        res.send("pong")})
+
+app.get('/items',(req,res)=> {
+    const sql = 'SELECT * FROM items;'
+    dataBase.query(sql,(err,results) => {if (err)
+        {return res.status(500).json({error:'Erreur du serveur'})}
+        
+        else {return res.status(200).json(results)}
+ })
+})
+
+app.post('/caca', (req, res) => {
+    const {name,prix,id_category,description}=req.body;
+        const sql = "INSERT INTO items (name, prix, id_category, description) VALUES ( ?, ?, ?, ?)";
+    
+        dataBase.query(sql,[name,prix,id_category,description], (err, results) => {
+            if (err) {
+                return res.status(500).json({ error: 'Erreur du serveur' });
+        } else {
+            return res.status(200).json(results)
+        }
+    });
+});
+
+app.put('/update', (req, res) => { 
+    const { id, name, prix, id_category, description } = req.body;
+    
+    const sql = "UPDATE items SET name = ?, prix = ?, id_category = ?, description = ? WHERE id = ?";
+    const values = [name, prix, id_category, description, id];
+
+    dataBase.query(sql, values, (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: 'Erreur du serveur' });
+        } else {
+            return res.status(200).json(results);
+        }
+    });
+});
+
+app.delete('/delete', (req, res) => {
+    const { id } = req.body;
+    
+    const sql = "DELETE FROM items WHERE id = ?";
+    const values = [id];
+
+    dataBase.query(sql, values, (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: 'Erreur du serveur' });
+        } else {
+            return res.status(200).json({ results });
+        }
+    });
+});
+
+app.post('/item-category', async (req, res) => {
+    const { name, categoryId } = req.body;
+    console.log('Données reçues :', req.body);
+    const query = "SELECT id FROM items WHERE name = ?;";
+    dataBase.query(query, [name], (err, results) => {
+        if (err) {return res.status(500).json({ error: 'Erreur lors de la recherche' });}
+        
+        const itemId = results[0].id; //extrait l'identifiant id de 1ere ligne du tableau results
+       
+        const insertQuery = "INSERT INTO item_category (item_id, category_id) VALUES (?, ?)";
+        dataBase.query(insertQuery, [itemId, categoryId], (insertErr) => {
+            if (insertErr) {return res.status(500).json({ error: 'Erreur du serveur lors de insertion de la relation',});}
+
+            return res.status(200).json({ message: 'Relation item-catégorie créée avec succès' });
+        });
+    });
+}); 
+
